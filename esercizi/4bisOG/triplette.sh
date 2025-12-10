2d8a52eda9a721a695f55bc8065f270b1e8f31db
jjacomo <giacomo.mengozzi4@studio.unibo.it>
2025-12-10 23:25:12 +0100

    prova ssh config

--- esercizi/4bisOG/triplette.sh
+++ esercizi/4bisOG/triplette.sh
@@ -2,7 +2,7 @@
 # (X;Y;Z) dove X Y e Z sono i nomi dei file, non nascosti, nella directory corrente
 
 NUM=0
-for FILE in /usr/include/*; do
+# for FILE in /usr/include/*; do
     # if ((NUM % 3 == 0)); then
     #     if ((NUM % 2 == 0)); then
     #         echo -n " ("
@@ -14,4 +14,9 @@ for FILE in /usr/include/*; do
     # echo "$FILE;"
     #
     # NAAAHHHHH
-done
+# done
+ls /usr/include/ > ./ciao.txt
+
+while read X Y Z ; do
+	echo "(${X};${Y};${Z})"
+done < ciao.txt
